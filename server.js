cb838e9a5f21f3bbde4da67693070104c8e71e67
Irwin Chen <irwin.chen@gmail.com>
2022-12-15 19:14:14 -0500

    Main files minus node-modules

---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://santamaria:QPRl9T9cBJ0DTnrc@cluster0.svytyoj.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const ActivitypubExpress = require('activitypub-express')
+const port = 8080;
+const app = express();
+const routes = {
+    actor: '/u/:actor',
+    object: '/o/:id',
+    activity: '/s/:id',
+    inbox: '/u/:actor/inbox',
+    outbox: '/u/:actor/outbox',
+    followers: '/u/:actor/followers',
+    following: '/u/:actor/following',
+    liked: '/u/:actor/liked',
+    collections: '/u/:actor/c/:id',
+    blocked: '/u/:actor/blocked',
+    rejections: '/u/:actor/rejections',
+    rejected: '/u/:actor/rejected',
+    shares: '/s/:id/shares',
+    likes: '/s/:id/likes'
+  };
+const apex = ActivitypubExpress({
+    name: 'Pastadon Cafe',
+    version: '1.0.0',
+    domain: 'localhost',
+    actorParam: 'actor',
+    objectParam: 'id',
+    activityParam: 'id',
+    routes,
+    endpoints: {
+      proxyUrl: 'https://localhost/proxy'
+    }
+  })
+  apex.createActor('system-user', 'System user', '', null, 'Service')
+  .then(async actor => {
+    await apex.store.saveObject(actor)
+    apex.systemUser = actor
+  })
+
+app.use(
+    express.json({ type: apex.consts.jsonldTypes }),
+    express.urlencoded({ extended: true }),
+    apex
+  );
+// define routes using prepacakged middleware collections
+app.route(routes.inbox)
+  .get(apex.net.inbox.get)
+  .post(apex.net.inbox.post)
+app.route(routes.outbox)
+  .get(apex.net.outbox.get)
+  .post(apex.net.outbox.post)
+app.get(routes.actor, apex.net.actor.get)
+app.get(routes.followers, apex.net.followers.get)
+app.get(routes.following, apex.net.following.get)
+app.get(routes.liked, apex.net.liked.get)
+app.get(routes.object, apex.net.object.get)
+app.get(routes.activity, apex.net.activityStream.get)
+app.get(routes.shares, apex.net.shares.get)
+app.get(routes.likes, apex.net.likes.get)
+app.get('/.well-known/webfinger', apex.net.webfinger.get)
+app.get('/.well-known/nodeinfo', apex.net.nodeInfoLocation.get)
+app.get('/nodeinfo/:version', apex.net.nodeInfo.get)
+app.post('/proxy', apex.net.proxy.post)
+// custom side-effects for your app
+app.on('apex-outbox', msg => {
+  if (msg.activity.type === 'Create') {
+    console.log(`New ${msg.object.type} from ${msg.actor}`)
+  }
+})
+app.on('apex-inbox', msg => {
+  if (msg.activity.type === 'Create') {
+    console.log(`New ${msg.object.type} from ${msg.actor} to ${msg.recipient}`)
+  }
+})
+
+
+// Database Name
+const dbName = 'myProject';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect()
+  .then(() => {
+    apex.store.db = client.db(dbName)
+    return apex.store.setup()
+  })
+  .then(() => {
+    app.listen(port, () => console.log(`apex app listening on port ${port}`))
+  })
+}
+
+main()
+  .then(console.log("Main..."))
+  .catch(console.error);
+  // .finally(() => client.close());
